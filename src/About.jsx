e6c306fcfe98931ca3dc71bd3b0da82aa59f1834
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-26 15:21:29 -0700

    Add About Component

---
+++ src/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+ 
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+ 
+  render() {
+    return (
+      <div>
+        This is where i will talk about myself.
+      </div>
+    );
+  }
+}
+export default About;
