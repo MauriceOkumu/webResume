e7a27edb622a4ec4111e09cf810f35d7e32df3da
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-26 15:30:55 -0700

    Add App component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,28 @@ console.log('Hello world');
 import React from 'react';
 import { render } from 'react-dom';
 import Counter from './Counter';
+
+ 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    	data: []
+    }
+  }
+ 
+  render() {
+    return (
+      <div>
+      <Counter />
+      </div>
+    );
+  }
+}
  
 document.addEventListener('DOMContentLoaded', function() {
   render(
-    <Counter />,
+    <App />,
     document.getElementById('mount')
   );
 });
