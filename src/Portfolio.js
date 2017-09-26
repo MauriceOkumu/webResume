7f48eb2a4c126bd8f7445bb3f6895883e22eb138
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-26 16:10:53 -0700

    Change file naming from.jsx to .js

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -6,17 +6,18 @@ class Portfolio extends React.Component {
   }
  
   render() {
-    this.props.map((item, index) => {
-
+   console.log(this.props.data)
+   let { title, url, src } = this.props.data
     return (
       <div>
         My Portfolio will be rendered and shown here.
-        <ul>
-          <li key={index}>{item}</li>
-        </ul>
+        <h1>{title}</h1>
+        <img src={url} alt="pictures of the works will be shown here" />
+        <br/>
+        <a href={src}>link to pictures</a>
       </div>
     );
-    })
+  
   }
 }
 export default Portfolio;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,25 @@ console.log('Hello world');
 import React from 'react';
 import { render } from 'react-dom';
 import Counter from './Counter';
-
+import Portfolio from './Portfolio.js';
+import About from './About.js'
  
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-    	data: []
+    	title:'Let us build a web app',
+    	url:'http://www.google.com',
+    	src:'http://www.codeacademy.com'
     }
   }
  
   render() {
     return (
       <div>
-      <Counter />
+        <Counter />
+        <About />
+        <Portfolio data={this.state}/>
       </div>
     );
   }
