f0a0d66ce65668f05ad4cbfd86568742182879e8
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-27 22:56:08 -0700

    Add home page

---
+++ src/Home.jsx
@@ -0,0 +1,18 @@
+import React,{ Component } from 'react';
+ 
+class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+ 
+  render() {
+   //let { title, url, src } = this.props.data
+    return (
+      <div>
+        <h3>My Home will be rendered and shown here.</h3>
+      </div>
+    );
+  
+  }
+}
+export default Home;
