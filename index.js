7ef3da9473c67e9def16594a92bbe8a8274e9b56
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-09-29 17:08:54 -0700

    Hot reload

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware')
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const path = require('path');
 const app = express();
+const port =  3000;
  
 const compiler = webpack(webpackConfig);
  
@@ -12,7 +14,6 @@ app.use(express.static(__dirname + '/static'));
 // app.get('*', function (request, response){
 //   response.sendFile(path.resolve(__dirname,'static', 'index.html'))
 // })
- 
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
@@ -21,9 +22,11 @@ app.use(webpackDevMiddleware(compiler, {
     colors: true,
   },
   historyApiFallback: true,
+  noInfo: true, publicPath: webpackConfig.output.publicPath
 }));
+app.use(webpackHotMiddleware(compiler)); 
  
-const server = app.listen(3000, function() {
+const server = app.listen(port, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
