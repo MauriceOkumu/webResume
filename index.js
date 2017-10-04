6535b49bd7192f7cd8da5b62692b8ab1c2f2682b
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-10-03 21:40:55 -0700

    Add dotenv

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const express = require('express');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const webpack = require('webpack');
-const webpackConfig = require('./webpack.config.prod.js');
+const webpackConfig = require('./webpack.config.js')();
 const path = require('path');
 const app = express();
 const port =  3000;
+require('dotenv').config()
  
 const compiler = webpack(webpackConfig);
  
 app.use(express.static(__dirname + '/static'));
 
-// app.get('*', function (request, response){
-//   response.sendFile(path.resolve(__dirname,'static', 'index.html'))
-// })
+
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
   filename: 'bundle.js',
@@ -30,5 +29,5 @@ const server = app.listen(port, function() {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
-   console.log('Environment var ',process.env.NODE_ENV)
 });
+   console.log('Environment var ',process.env.NODE_ENV)
