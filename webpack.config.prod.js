b0e8eb83e4817b7fb0570cb22ab2b3e9f8825a1b
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-10-05 12:11:49 -0700

    Configure production webpack build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,12 +1,17 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const common = require('./webpack.config.js')('dev');
+const common = require('./webpack.config.dev.js')();
+// require('dotenv').config();
 
 module.exports = merge(common, {
 	devtool: 'cheap-module-source-map',
 	plugins: [
-	new UglifyJSPlugin({
+	new webpack.optimize.UglifyJsPlugin({
+		compress: {
+          screw_ie8: true,
+          warnings: false
+        },
 		sourceMap: true
 	}),
 	new webpack.DefinePlugin({
