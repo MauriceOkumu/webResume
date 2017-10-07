b65a72cca88bc7b1cad2d2a1c995fad526912702
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-10-06 19:42:09 -0700

    Test image loading

--- src/About.jsx
+++ src/About.jsx
@@ -1,4 +1,5 @@
 import React,{ Component } from 'react';
+import Pic from './IMG_1160.jpg'
  
 class About extends Component {
   constructor(props) {
@@ -8,7 +9,8 @@ class About extends Component {
     return (
       <div>
         This is where i will talk about myself.
-        Will go on and on
+        Will go on and on<br/>
+        <img src={Pic} alt="Family picture" />
       </div>
     );
   }
