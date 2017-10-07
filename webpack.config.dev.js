8951776ad74f0ed9dd2ab82feb1dcb041e21f253
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-10-06 19:42:56 -0700

    Add image loaders to webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -40,7 +40,8 @@ module.exports = () => {
             }
           }
         ])
-      }
+      },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }
     ],
   },
   devtool: 'inline-source-map',
