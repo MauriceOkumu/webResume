49b33e64a9572bb96d39baa0e32f93598caa6971
Maurice Okumu <ochiemaurice@yahoo.co.uk>
2017-10-27 14:18:41 -0700

    code clean up

--- backend/database.js
+++ backend/database.js
@@ -1,18 +1,10 @@
 const mysql = require('mysql');
  const connection = mysql.createConnection({
  	host: 'localhost',
- 	user: 'websiteData',
+ 	user: 'root',
  	password: '',
  	database: 'website'
  });
 
- connection.conect()
-
- connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
- 	if (err) {
- 		throw err;
- 	}
- 	console.log('The solution is : =>', rows[0].solution);
- });
 
  module.exports = connection;
